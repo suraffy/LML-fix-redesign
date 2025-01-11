ea38022dadb7c221c2f3ebb4722b23c160fc3fb6
suraffy <surafelay@gmail.com>
2025-01-11 23:05:11 +0300

    Build a navigation bar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,21 +1,22 @@
 'use client';
 
+import React, { forwardRef, AnchorHTMLAttributes, ReactNode, useState, useEffect } from 'react';
+import * as NavigationMenu from '@radix-ui/react-navigation-menu';
+import classNames from 'classnames';
+import { CaretDownIcon } from '@radix-ui/react-icons';
 import Image from 'next/image';
 import Link from 'next/link';
 import logo from '@/public/lml_logo.png';
-import {
-  HoverCard,
-  HoverCardContent,
-  HoverCardTrigger,
-} from '../ui/hover-card';
-import { useState } from 'react';
-import { Button } from '../ui/button';
-import { Menu } from 'lucide-react';
+import { FolderCode, Droplet, SearchCheck, Key, DollarSign, Settings, Tag } from 'lucide-react';
+import { Button, Flex } from '@radix-ui/themes';
+import * as RadixColors from '@radix-ui/colors';
 
 interface SubLink {
   id: number;
   title: string;
   link: string;
+  icon?: JSX.Element;
+  description?: string;
 }
 
 interface NavLink {
@@ -23,23 +24,85 @@ interface NavLink {
   title: string;
   link: string;
   hover: boolean;
+  image?: boolean;
   subLinks?: SubLink[];
 }
 
 const navLinks: NavLink[] = [
   { id: 1, title: 'Home', link: '/', hover: false },
-  // { id: 2, title: 'Services', link: '/services', hover: false },
+  {
+    id: 2,
+    title: 'Services',
+    link: '/services',
+    hover: true,
+    subLinks: [
+      {
+        id: 1,
+        title: 'Software',
+        link: '/software',
+        icon: <FolderCode />,
+        description: 'Fix and update',
+      },
+      {
+        id: 2,
+        title: 'Diagnostics',
+        link: '/diagnostics',
+        icon: <SearchCheck />,
+        description: 'Identify and fix device issues',
+      },
+      {
+        id: 3,
+        title: 'Cleaning',
+        link: '/cleaning',
+        icon: <Droplet />,
+        description: 'Deep clean',
+      },
+      {
+        id: 4,
+        title: 'Unlocks',
+        link: '/unlocks',
+        icon: <Key />,
+        description: 'Unlock your devices',
+      },
+      {
+        id: 5,
+        title: 'Buybacks',
+        link: '/buybacks',
+        icon: <DollarSign />,
+        description: 'Sell your old devices',
+      },
+      {
+        id: 6,
+        title: 'Customization',
+        link: '/customization',
+        icon: <Settings />,
+        description: 'Personalize your device',
+      },
+    ],
+  },
   {
     id: 3,
     title: 'Location',
     link: '#',
     hover: true,
+    image: true,
     subLinks: [
-      { id: 4, title: 'Seattle', link: '/seattle' },
-      { id: 5, title: 'West Seattle', link: '/westseattle' },
-      { id: 6, title: 'North Seattle', link: '/northseattle' },
+      { id: 1, title: 'Seattle', link: '/seattle', description: 'Serving all of Seattle' },
+      {
+        id: 2,
+        title: 'West Seattle',
+        link: '/westseattle',
+        description: 'Reliable service in West Seattle',
+      },
+      {
+        id: 3,
+        title: 'North Seattle',
+        link: '/northseattle',
+        description: 'Convenient repairs in North Seattle',
+      },
     ],
   },
+  { id: 4, title: 'FAQ', link: '/faq', hover: false },
 ];
 
 const NavBar: React.FC = () => {
@@ -55,99 +118,159 @@ const NavBar: React.FC = () => {
     setExpandedMenu((prev) => (prev === id ? null : id));
   };
 
+  const [isSticky, setIsSticky] = useState(false);
+
+  const handleScroll = () => {
+    const currentScrollY = window.scrollY;
+    setIsSticky(currentScrollY > 20);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className='flex items-center justify-between relative sm:mx-20 mt-4 border-b pb-2 mb-4'>
-      {/* Logo */}
-      <Link href='/' onClick={handleClickLink}>
-        <Image
-          src={logo}
-          width={60}
-          height={60}
-          loading='lazy'
-          alt='logo'
-          className='block transition-transform duration-150 rounded-xl hover:scale-110 mx-2'
-        />
-      </Link>
+    <div className="shadow-sm">
+      <NavigationMenu.Root className="flex items-end justify-between relative bg-white dark:bg-gray2 dark:bg-gray2 sm:mx-20 py-2">
+        {/* Logo */}
+        <Link href="/" onClick={handleClickLink} className="w-16">
+          <Image
+            src={logo}
+            width={50}
+            height={50}
+            loading="lazy"
+            alt="logo"
+            className="block mx-auto rounded-xl"
+            style={{
+              width: isSticky ? '40px' : '60px',
+              height: isSticky ? '40px' : '60px',
+              borderRadius: isSticky ? '50%' : '',
+              transition: 'all 0.2s ease',
+            }}
+          />
+        </Link>
 
-      {/* Navigation Links for Large Screens */}
-      <ul className='hidden sm:flex items-center gap-8 xl:gap-10 font-semibold'>
-        {navLinks.map((nav) => (
-          <li key={nav.id}>
-            {nav.hover ? (
-              <HoverCard>
-                <HoverCardTrigger asChild>
-                  <span className='underline-animation text-lg hover:underline-offset-1 hover:border-b hover:border-[#e3de1e]'>
-                    {nav.title}
-                  </span>
-                </HoverCardTrigger>
-                <HoverCardContent className='w-screen my-4'>
-                  <div className='text-right pr-16 space-y-2'>
-                    {nav.subLinks?.map((subLink) => (
-                      <li key={subLink.id}>
-                        <Link href={subLink.link}>
-                          <span className='underline-animation text-lg hover:underline-offset-1 hover:border-b hover:border-[#e3de1e]'>
-                            {subLink.title}
-                          </span>
-                        </Link>
-                      </li>
-                    ))}
-                  </div>
-                </HoverCardContent>
-              </HoverCard>
-            ) : (
-              <Link href={nav.link}>
-                <span className='underline-animation text-lg hover:underline-offset-1 hover:border-b hover:border-[#e3de1e]'>
-                  {nav.title}
-                </span>
-              </Link>
-            )}
-          </li>
-        ))}
-      </ul>
-
-      {/* Mobile Navigation */}
-      <div className='sm:hidden mx-2'>
-        <Button onClick={() => setOpenNavbar((pre) => !pre)}>
-          <Menu />
-        </Button>
-      </div>
-
-      {openNavbar && (
-        <ul className='sm:hidden absolute bg-white w-full mt-44 z-50 py-5 rounded-lg px-3 space-y-2'>
+        <NavigationMenu.List className="hidden m-0 sm:flex items-end p-0 gap-4 xl:gap-6 font-semibold">
           {navLinks.map((nav) => (
-            <li key={nav.id} className='space-y-2'>
-              <span
-                className='underline-animation p-0.5 text-lg hover:underline-offset-1 hover:border-b hover:border-[#e3de1e] flex justify-between'
-                onClick={() =>
-                  nav.hover ? toggleSubMenu(nav.id) : handleClickLink()
-                }
-              >
-                {nav.title}
-                {nav.hover && (
-                  <span className='ml-2'>
-                    {expandedMenu === nav.id ? '▲' : '▼'}
-                  </span>
-                )}
-              </span>
-              {nav.hover && expandedMenu === nav.id && (
-                <ul className='pl-4'>
-                  {nav.subLinks?.map((subLink) => (
-                    <li key={subLink.id}>
-                      <Link href={subLink.link} onClick={handleClickLink}>
-                        <span className='underline-animation text-lg hover:underline-offset-1 hover:border-b hover:border-[#e3de1e]'>
-                          {subLink.title}
-                        </span>
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
+            <NavigationMenu.Item key={nav.id}>
+              {nav.hover ? (
+                <>
+                  <NavigationMenu.Trigger className="group flex select-none items-center justify-between gap-0.5 rounded px-4 py-3 text-[16px] font-medium leading-none text-violet11 outline-none hover:bg-violet3 focus:shadow-[0_0_0_2px] focus:shadow-violet7">
+                    {nav.title}
+                    <CaretDownIcon
+                      className="relative top-px text-violet11 transition-transform duration-[250] ease-in group-data-[state=open]:-rotate-180"
+                      aria-hidden
+                    />
+                  </NavigationMenu.Trigger>
+                  <NavigationMenu.Content className="absolute left-0 top-0 w-full data-[motion=from-start]:animate-enterFromRight data-[motion=from-end]:animate-enterFromLeft data-[motion=to-start]:animate-exitToRight data-[motion=to-end]:animate-exitToLeft sm:w-auto">
+                    <ul
+                      className={`one m-0 grid list-none gap-x-2.5 p-[22px] sm:w-[500px] sm:grid-cols-[0.75fr_1fr] ${!nav.image ? 'm-0 grid list-none gap-x-2.5 p-[22px] sm:w-[600px] sm:grid-flow-col sm:grid-rows-3' : ''}`}
+                    >
+                      {nav.image && (
+                        <li className="row-span-3 grid">
+                          <NavigationMenu.Link asChild>
+                            <Link
+                              href="/"
+                              onClick={handleClickLink}
+                              className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-[#fef100] to-gray-800 p-[25px] no-underline outline-none focus:shadow-[0_0_0_2px] focus:shadow-violet7"
+                            >
+                              <Image
+                                src={logo}
+                                width={60}
+                                height={60}
+                                loading="lazy"
+                                alt="logo"
+                                className="block transition-transform duration-150 rounded-xl hover:scale-110 mx-2"
+                              />
+
+                              <div className="mb-[7px] mt-4 text-[18px] font-medium leading-[1.2] text-white">
+                                LML Fix
+                              </div>
+                              <p className="text-[14px] leading-[1.3] text-mauve4l text-gray-50">
+                                Fast and affordable, around seattle.
+                              </p>
+                            </Link>
+                          </NavigationMenu.Link>
+                        </li>
+                      )}
+
+                      {nav.subLinks?.map((subLink) => (
+                        <ListItem key={subLink.id} href={subLink.link} title={subLink.title}>
+                          <div className="inline-flex gap-2">
+                            {subLink.icon} {subLink.description}
+                          </div>
+                        </ListItem>
+                      ))}
+                    </ul>
+                  </NavigationMenu.Content>
+                </>
+              ) : (
+                <NavigationMenu.Link
+                  className="block select-none rounded px-4 py-3 text-[16px] font-medium leading-none text-violet11 no-underline outline-none hover:bg-violet3 focus:shadow-[0_0_0_2px] focus:shadow-violet7"
+                  href={nav.link}
+                >
+                  {nav.title}
+                </NavigationMenu.Link>
               )}
-            </li>
+            </NavigationMenu.Item>
           ))}
-        </ul>
-      )}
-    </nav>
+
+          <NavigationMenu.Indicator className="top-full z-10 flex h-2.5 items-end justify-center overflow-hidden transition-[width,transform_250ms_ease] data-[state=hidden]:animate-fadeOut data-[state=visible]:animate-fadeIn">
+            <div className="relative top-[70%] size-3 rotate-45 rounded-tl-xsl bg-violet4" />
+          </NavigationMenu.Indicator>
+        </NavigationMenu.List>
+
+        <div className="perspective-[2000px] absolute left-0 top-full flex w-full justify-center">
+          <NavigationMenu.Viewport className="shadow-[0_2px_10px] shadow-gray4 border border-gray3 relative mt-0.5 h-[var(--radix-navigation-menu-viewport-height)] w-full origin-[top_center] overflow-hidden rounded-md bg-white dark:bg-gray2 transition-[width,_height] duration-300 data-[state=closed]:animate-scaleOut data-[state=open]:animate-scaleIn sm:w-[var(--radix-navigation-menu-viewport-width)]" />
+        </div>
+
+        <Flex gap="3" align="center">
+          <Button size="3">
+            <Link href="/" onClick={handleClickLink} className="flex gap-2 items-center py-2">
+              <Tag size={20} /> Book Now
+            </Link>
+          </Button>
+
+          <Button size="3" color="gray" highContrast>
+            <Link href="/" onClick={handleClickLink} className="py-2">
+              Contact Us
+            </Link>
+          </Button>
+        </Flex>
+      </NavigationMenu.Root>
+    </div>
   );
 };
+
+type ListItemProps = {
+  className?: string;
+  children: ReactNode;
+  title: string;
+} & AnchorHTMLAttributes<HTMLAnchorElement>;
+
+const ListItem = forwardRef<HTMLAnchorElement, ListItemProps>(
+  ({ className, children, title, ...props }, forwardedRef) => (
+    <li>
+      <NavigationMenu.Link asChild>
+        <a
+          className={classNames(
+            'block select-none rounded-md p-3 text-[15px] leading-none no-underline outline-none transition-colors hover:bg-mauve3 focus:shadow-[0_0_0_2px] focus:shadow-violet7',
+            className
+          )}
+          {...props}
+          ref={forwardedRef}
+        >
+          <div className="mb-[5px] font-medium leading-[1.2] text-violet12">{title}</div>
+          <div className="leading-[1.4] text-mauve11">
+            <div className="inline-flex gap-2">{children}</div>
+          </div>
+        </a>
+      </NavigationMenu.Link>
+    </li>
+  )
+);
 
 export default NavBar;
