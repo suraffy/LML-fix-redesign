cb776f116a3983e714995b46cfe0a1ed002d7747
suraffy <surafelay@gmail.com>
2025-01-11 23:05:27 +0300

    Integrate Radix UI themes and enhance layout with a promotional banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import '@radix-ui/themes/styles.css';
+import { Button, Theme, ThemePanel } from '@radix-ui/themes';
 import Navbar from '@/components/Navbar/Navbar';
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,10 +19,32 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        {children}
+        <Theme
+          appearance="light"
+          accentColor="yellow"
+          grayColor="sand"
+          radius="large"
+          scaling="95%"
+        >
+          <div
+            className={`hidden sm:flex gap-3 items-center justify-center bg-[#FEF100] text-gray-900 text-center text-sm font-medium tracking-wide`}
+          >
+            <span className="py-3">Reliable Repairs for Your Phones, Laptops, and Electronics</span>
+            <Button size="2" style={{ backgroundColor: '#41431f', color: 'white' }}>
+              <Link href="/" className="tracking-wide">
+                Learn More
+              </Link>
+            </Button>
+          </div>
+
+          <div className="sticky top-0 z-50">
+            <Navbar />
+          </div>
+          {children}
+          {/* <ThemePanel /> */}
+        </Theme>
       </body>
     </html>
   );
