a5cc516a0ba63e89b7ec24a1bb5ce336b6fe409e
suraffy <surafelay@gmail.com>
2025-01-14 15:40:44 +0300

    Refactor layout to include Footer component and update Theme properties for improved styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,10 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
 import '@radix-ui/themes/styles.css';
-import { Button, Theme } from '@radix-ui/themes';
+import { Theme } from '@radix-ui/themes';
 import Navbar from '@/components/Navbar/Navbar';
-import Link from 'next/link';
+import Footer from '@/components/Footer/Footer';
+
 import { useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -27,26 +28,19 @@ export default function RootLayout({
         <Theme
           appearance={themeAppearance}
           accentColor="yellow"
-          grayColor="sand"
-          radius="large"
+          grayColor="mauve"
+          radius="medium"
           scaling="95%"
         >
-          <div
-            className={`hidden lg:flex gap-3 items-center justify-center bg-[#FEF100] text-gray-900 text-center text-sm font-medium tracking-wide`}
-          >
-            <span className="py-3">Reliable Repairs for Your Phones, Laptops, and Electronics</span>
-            <Button size="2" style={{ backgroundColor: '#41431f', color: 'white' }}>
-              <Link href="/" className="tracking-wide">
-                Learn More
-              </Link>
-            </Button>
-          </div>
+          <div className={inter.className}>
+            <div className="sticky top-0 z-50">
+              <Navbar theme={themeAppearance} toggleTheme={toggleTheme} />
+            </div>
 
-          <div className="sticky top-0 z-50">
-            <Navbar theme={themeAppearance} toggleTheme={toggleTheme} />
-          </div>
+            {children}
 
-          {children}
+            <Footer />
+          </div>
         </Theme>
       </body>
     </html>
